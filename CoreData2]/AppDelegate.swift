4b5f04b67637443b7ecd136ce1f5396969a69a65
COE-05 <coe-05@coe-05s-imac.local>
2020-01-17 15:24:14 +0530

    DONE

--- CoreData2]/AppDelegate.swift
+++ CoreData2]/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        let docPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        print(docPath)
         // Override point for customization after application launch.
         return true
     }
--- CoreData2]/ViewController.swift
+++ CoreData2]/ViewController.swift
@@ -7,9 +7,62 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-
+    
+    
+    var UserData: [NSManagedObject] = [] 
+    
+    @IBOutlet weak var Email: UITextField!
+    
+    
+    @IBOutlet weak var Name: UITextField!
+    
+    
+    @IBAction func Savebtn(_ sender: UIButton) {
+       self.save(email: Email.text!, name: Name.text!)
+        
+        
+    }
+    
+  
+    
+    func save(email:String, name:String) {
+        
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        // 1
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        // 2
+        let entity =
+            NSEntityDescription.entity(forEntityName: "Urvi",
+                                       in: managedContext)!
+        
+        let person = NSManagedObject(entity: entity,
+                                     insertInto: managedContext)
+        
+        // 3
+        person.setValue(Email.text, forKeyPath: "email")
+        person.setValue(Name.text, forKey: "name")
+        
+        
+        // 4
+        do {
+            try managedContext.save()
+            UserData.append(person)
+            
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
